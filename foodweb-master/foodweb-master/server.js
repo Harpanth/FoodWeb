9f208b5bfbbe1b418af183f99d24b2d926c5566d
Harpanth <panthdhillon6@gmail.com>
2021-12-16 12:20:12 +0530

    First commit

---
+++ foodweb-master/foodweb-master/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const Dish = require("./modals/userSchema");
+const app = express();
+
+mongoose.connect('mongodb+srv://subodh:Sub@123@cluster0.fclox.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db =mongoose.connection;
+db.on("error",console.error.bind(console,"connection error:"));
+db.once("open",()=>{
+    console.log("Database connected");
+});
+
+app.use(bodyParser.json());
+app.use(cors({origin:"http://localhost:3000"}))
+
+app.get('/dish',async (req,res)=>{
+    const dishes = await Dish.find({});
+    res.send(dishes);
+   
+})
+
+
+if(process.env.NODE_ENV =="production"){
+    app.use(express.static("client/build"));
+}
+
+
+app.listen(process.env.PORT||3030,()=>{
+    console.log("server running on port 3030");
+})
