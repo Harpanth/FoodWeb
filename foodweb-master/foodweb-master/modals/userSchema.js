1bd54f2c74009f73b64b351f334bd1ece060dbaf
Harpanth <panthdhillon6@gmail.com>
2021-12-16 12:51:10 +0530

    first commit

--- foodweb-master/foodweb-master/modals/userSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const UserSchema = new mongoose.Schema({
-    name:String,
-    price:Number
-})
-
-const Dish = mongoose.model("Dish",UserSchema);
-module.exports = Dish;
